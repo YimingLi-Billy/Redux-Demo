3d918217af4b836aee8d2618416be0f0282afcb8
Yiming Li <ysqj1hg@gmail.com>
2023-04-25 02:02:42 -0400

    Redux Demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Todo from "./Todo";
 
-function App() {
+const App = () => {
+  const [content, setContent] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    setContent("");
+    e.preventDefault();
+    setTodos((pre) => {
+      return [...pre, { id: new Date().getTime(), content: content }];
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+        <input disabled={content === ""} type="submit" value="Submit" />
+      </form>
+      <div>
+        {todos.map((todo, id) => (
+          <Todo key={id} task={todo} setTodos={setTodos} id={todo.id} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
